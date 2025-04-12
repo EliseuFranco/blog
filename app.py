279f0bb8e49c13c43a704f70b4526bc008902723
Eliseu Samulolo <eliseusamulolo@gmail.com>
2025-04-12 17:39:53 +0100

    Removi o debug=true e adicionei a porta como variável de ambiente

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ app.register_blueprint(author_bp, url_prefix = "/user") #registo da rota de usu√
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port="5000")
+    import os
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)

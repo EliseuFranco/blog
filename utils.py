4e638c9f6bb82e6b27dbe3ab7a1a9f2248087590
Eliseu Samulolo <eliseusamulolo@gmail.com>
2025-04-12 22:22:23 +0100

    variável de ambiente url database

--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ ALLOWED_FILES = ["png",'jpg','jpg']
 
 def check_file(file):
     return "." in file and file[file.index(".")+1:].lower() in ALLOWED_FILES
+
+import os
+
+print(os.environ.get('ps_data_base_url'))

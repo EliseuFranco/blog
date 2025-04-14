e6b0c17c938949eaf2e78d3a5fc6efa4662ad3ce
Eliseu Samulolo <eliseusamulolo@gmail.com>
2025-04-14 01:33:36 +0100

    remoção da url da base de dados do código

--- utils.py
+++ utils.py
@@ -5,6 +5,4 @@ ALLOWED_FILES = ["png",'jpg','jpg']
 def check_file(file):
     return "." in file and file[file.index(".")+1:].lower() in ALLOWED_FILES
 
-import os
 
-print(os.environ.get('ps_data_base_url'))
